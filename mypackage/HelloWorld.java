ddf3a1c0c3360d0e4403cf3fe4698dbabc2951c4
jianxun <jianxunbak@gmail.com>
2024-11-01 18:54:59 +0800

    completed assignment 3.4

--- mypackage/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package mypackage;
-// when put java files inside folders, we need to add the package keyword
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World from my package");
-    }
-}
